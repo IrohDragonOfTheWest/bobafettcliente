60c8021c16485e63ab8e316a813376bd6c520142
MauryAC <abundezcruzmaury@gmail.com>
2024-04-15 23:03:43 -0600

    Agregar Productos al Carrito

---
+++ src/components/utils/Store.js
@@ -0,0 +1,59 @@
+import { useReducer, createContext } from "react"; 
+
+export const Store = createContext();
+
+const initialState={
+    cart : {
+        cartItems:[],
+    }
+}
+// /funcion reductora donde se crea la logica funcional 
+function reducer(state, action){
+        switch (action.type) {
+            case 'ADD_TO_CART': {
+                const newItem = action.payload;
+                const existItemIndex = state.cart.cartItems.findIndex(item => item.id === newItem.id);
+              
+                if (existItemIndex !== -1) {
+                  // Si el artículo ya está en el carrito, incrementa su cantidad
+                  const updatedCartItems = [...state.cart.cartItems];
+                  updatedCartItems[existItemIndex].quantity += newItem.quantity;
+                  return { ...state, cart: { ...state.cart, cartItems: updatedCartItems } };
+                } else {
+                  // Si el artículo no está en el carrito, agrégalo
+                  return { ...state, cart: { ...state.cart, cartItems: [...state.cart.cartItems, newItem] } };
+                }
+              }
+              
+case 'CART_REMOVE_ITEM' :{
+    const cartItems = state.cart.cartItems.filter(
+        (item => item.id !== action.payload.id)
+    )
+    return {...state, cart:{...state.cart,cartItems} }
+}
+
+case 'REMOVE_CART':{
+    const cartItems = []
+    return {...state, cart:{...state.cart,cartItems} }
+}
+case 'INCREMENT_QUANTITY': {
+    const { id } = action.payload;
+    const cartItems = state.cart.cartItems.map(item => {
+      if (item.id === id) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: { ...state.cart, cartItems } };
+  }
+  
+default:
+    return state;
+}
+}
+
+export function StoreProvieder({children}){
+    const [state, dispatch] = useReducer(reducer, initialState)
+    
+    return <Store.Provider value={{state, dispatch}}>{children}</Store.Provider>
+}
