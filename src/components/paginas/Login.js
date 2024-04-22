a0978c501c85c42ad384109d0b3c6af434e05e37
Yersai Mendez <yersaimendez285@gmail.com>
2024-04-21 20:40:47 -0600

    Funcionamiento de agregar usuarios con roles

--- src/components/paginas/Login.js
+++ src/components/paginas/Login.js
@@ -1,13 +1,54 @@
 import React, { useState } from 'react'
+import firebase from '../../firebase/firebase';
+import { getAuth, createUserWithEmailAndPassword, signOut, signInWithEmailAndPassword } from 'firebase/auth';
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
+
+const auth = firebase.auth;
+const firestore = getFirestore(firebase.firestore);
 
 function Login() {
 
     const [isRegistrando, setIsRegistrando] = useState(false);
+
+    async function registrarUsuario(email, password, rol) {
+        const infoUsuario = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        ).then((usuarioFirebase) => {
+            return usuarioFirebase;
+        });
+
+        console.log(infoUsuario.user.uid);
+
+        const docuRef = doc(firestore, `usuarios/${infoUsuario.user.uid}`);
+
+        setDoc(docuRef, { correo: email, rol: rol });
+    }
+
+    function submitHandler(e) {
+        e.preventDefault();
+
+        const email = e.target.elements.email.value;
+        const password = e.target.elements.password.value;
+        const rol = e.target.elements.rol.value;
+
+        console.log("submit", email, password, rol)
+
+        if (isRegistrando) {
+            //registrar
+            registrarUsuario(email, password, rol);
+        } else {
+            //login
+            signInWithEmailAndPassword(auth, email, password)
+
+        }
+    }
     return (
         <div>
             <h1>{isRegistrando ? "Regístrate" : "Inicia sesión"}</h1>
 
-            <form>
+            <form onSubmit={submitHandler}>
                 <label>
                     Correo electrónico:
                     <input type="email" id="email" placeholder="Ingrese su correo" />
@@ -20,20 +61,25 @@ function Login() {
 
                 <label>
                     Rol:
-                    <select>
+                    <select id="rol">
                         <option value="admin">Administrador</option>
                         <option value="user">Usuario</option>
                     </select>
                 </label>
 
                 <input
-                type="submit" 
-                value={isRegistrando ? "Registrar" : "Iniciar sesión"}
+                    type="submit"
+                    value={isRegistrando ? "Registrar" : "Iniciar sesión"}
                 />
             </form>
 
             <button onClick={() => setIsRegistrando(!isRegistrando)} className="text-blue-500 underline text-sm">
                 {isRegistrando ? "Ya tengo una cuenta" : "Quiero registarme"}
+            </button>
+
+            <br />
+            <button onClick={() => signOut(auth)}>
+                cerrar sesión
             </button>
         </div>
     )
--- src/components/ui/Sidebar.js
+++ src/components/ui/Sidebar.js
@@ -3,53 +3,54 @@ import { NavLink } from "react-router-dom";
 import '../../css/styles.css';
 import logo from '../imagenes/logo.jpeg'
 
-const Sidebar = ({ isOpen }) => {
+import { signOut } from "firebase/auth";
+import firebase from "../../firebase/firebase";
+
+const auth = firebase.auth;
+
+const Sidebar = () => {
 
     const barStyle = {
         backgroundColor: '#624100',
-        width: isOpen ? '20rem' : '0',
-        transition: 'width 0.5s ease'
     };
 
     return (
 
         <div className="md: w-2/5 xl:w-1/5" style={barStyle}>
-            {isOpen && (
-                <div className="p-6">
-                    <p className="uppercase text-white text-2xl tracking-wide text-center font bold">
-                        BobaFett App
-                    </p>
-                    <div className="logo-container">
-                        <img className="logo " src={logo} alt="logo" />
-                    </div>
-
-                    <p className="mt-3 text-gray-500">
-                        Administar tu negocio en las siguientes opciones
-                    </p>
-                    <nav>
-
-                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/">
-                            Inicio
-                        </NavLink>
-
-                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/ordenes">
-                            Ordenes
-                        </NavLink>
-
-                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/menu">
-                            Menu
-                        </NavLink>
-
-                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/inventario">
-                            Inventario
-                        </NavLink>
-
-                        <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/login">
-                            Login
-                        </NavLink>
-                    </nav>
+            <div className="p-6">
+                <p className="uppercase text-white text-2xl tracking-wide text-center font bold">
+                    BobaFett App
+                </p>
+                <div className="logo-container">
+                    <img className="logo " src={logo} alt="logo" />
                 </div>
-            )}
+
+                <p className="mt-3 text-gray-500">
+                    Administar tu negocio en las siguientes opciones
+                </p>
+                <nav>
+
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/">
+                        Inicio
+                    </NavLink>
+
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/ordenes">
+                        Ordenes
+                    </NavLink>
+
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/menu">
+                        Menu
+                    </NavLink>
+
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/inventario">
+                        Inventario
+                    </NavLink>
+
+                    <NavLink className="p-1 text-gray-400 block hover:bg-yellow-500 hover:text-gray-900" activeclassname="text-yellow-500" exact="true" to="/login">
+                        Login
+                    </NavLink>
+                </nav>
+            </div>
         </div>
 
     );
